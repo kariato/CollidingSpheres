74d67f8e8406d0de753afebba8393eba1b5e8f66
cheech36 <chich36@gmail.com>
2016-03-19 00:27:32 -0400

    Add Neural Network and SmartPlayer class

---
+++ fullANN.py
@@ -0,0 +1,3 @@
+class neuralNetwork:
+    def __init__(self):
+        pass
--- smartPlayer.py
+++ smartPlayer.py
@@ -1,48 +1,10 @@
 from player import *
-from perceptron import perceptron
+from fullANN import *
 
+class smartPlayer (player):
 
-class smartPlayer(player):
-    def __init__(self, position, nInputs, id = 'none'):
+    def __init__(self, position, id):
         player.__init__(self, position, id)
-
-        self.nInputs = nInputs
-        self.brain  = perceptron(nInputs,.1)
-        self.target = list()
-        self.error  = vector(0,0,)
-
-    def chase(self): ##Seek(ArrayList targets)
-        desired = vector(self.position - self.target[0])
-        desired = norm(desired)*self.maxSpeed
-        steer   = vector(desired - self.velocity)
-        if steer.mag > self.maxSpeed:
-            steer.mag = self.maxSpeed
-        return steer
-
-    def steer(self):
-
-            if len(self.target) != 0:
-                forces = list()
-                for n in range(0, self.nInputs):
-                    forces.append(self.chase())
-                result = vector(self.brain.feedForward(forces))## Brain calculates 1 new force
-                self.setForce(result)
-                self.error = vector(self.target[0]- self.position)
-                self.brain.train(forces, self.error)
-            else:
-                print('No target Set')
-
-            ##if( ) steering procedures for other tasks
-
-    def setTarget(self, targetPos):
-        self.target.append(targetPos)
+        self.brain = neuralNetwork()
 
 
-    def printStats(self):
-        print('PlayerID: ' , self.id )
-        print(' Position', self.getPosition() )
-        print('velocity', self.getVelocity() )
-        print('accelertaion', self.getAcceleration())
-        print('Player Bottom', self.getBottom())
-        print('weight', self.brain.weights)
-        print('error', self.error)
